5be240f72e0a2d946a7090cf76a44a2d915d13b3
bluluke <bluluke@hotmail.com>
2023-11-20 20:39:09 +0000

    Update Select Books: Read books from 1900s.

--- db/04-select-books.sql
+++ db/04-select-books.sql
@@ -6,3 +6,7 @@ SELECT title FROM bookshop_inventory;
 \echo '\n Here are all the non-fiction books:\n'
 SELECT title FROM bookshop_inventory
 WHERE is_fiction = false; 
+
+\echo '\n Here are all the books released in the 1900s:\n'
+SELECT title FROM bookshop_inventory
+WHERE release_date BETWEEN '1900=01-01' AND '1999-12-31'; 
