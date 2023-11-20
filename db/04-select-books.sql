af6ff926bd9e40948d362540d76b0d9077debd7e
bluluke <bluluke@hotmail.com>
2023-11-20 20:35:27 +0000

    Update Select Books Non-fiction.

--- db/04-select-books.sql
+++ db/04-select-books.sql
@@ -2,3 +2,7 @@
 
 \echo '\n Here are all the books that we have in stock:\n'
 SELECT title FROM bookshop_inventory;
+
+\echo '\n Here are all the non-fiction books:\n'
+SELECT title FROM bookshop_inventory
+WHERE is_fiction = false; 
