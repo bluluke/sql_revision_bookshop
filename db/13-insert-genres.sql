fc9bd3503706ff9f9fd21d0f7cb105c71c487e8a
bluluke <bluluke@hotmail.com>
2023-11-22 21:07:14 +0000

    Add Insert genres.

---
+++ db/13-insert-genres.sql
@@ -0,0 +1,13 @@
+\c my_bookshop_revisited;
+
+INSERT INTO genre   
+    (genre_name)
+VALUES 
+    ('science fiction'),
+    ('children''s'),
+    ('romance'),
+    ('fantasy'),
+    ('dystopian'),
+    ('science'),
+    ('adventure'),
+    ('classics');
