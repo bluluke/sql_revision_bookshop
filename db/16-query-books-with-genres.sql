905fd6325a0206023fc39a37039e6bb4d6fad3ed
bluluke <bluluke@hotmail.com>
2023-11-23 19:40:21 +0000

    Add Read Dystopian Novels.

--- db/16-query-books-with-genres.sql
+++ db/16-query-books-with-genres.sql
@@ -8,5 +8,10 @@ JOIN book_genre
 On genre.genre_id = book_genre.genre_id
 WHERE book_genre.book_id = 4;
 
+\echo '\n Here are all the dystopian books:\n'
 
-
+SELECT title 
+FROM bookshop_inventory
+JOIN book_genre
+ON book_genre.book_id = bookshop_inventory.book_id
+WHERE book_genre.genre_id = 5;
